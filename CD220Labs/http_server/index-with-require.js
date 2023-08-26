b3dbe39e8036c2232d07e362a100bb5d547c6c6e
naim-2 <121217709+naim-2@users.noreply.github.com>
2023-08-27 01:59:25 +0300

    Add greetings

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -3,7 +3,21 @@ const today = require('./today');
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
+  let hr = today.getDate().getHours();
+  let greet = "Early morning!"; 
+  if(hr > 6 && hr < 12){
+      greet = "Good morning!";      
+  }
+  else if(hr > 12 && hr < 18){
+      greet = "Good afternoon";
+  }
+  else if(hr > 18 && hr < 21){
+      greet = "Good evening";
+  }
+  else if(hr > 21 && hr < 24){
+      greet = "Good night";
+  }
+  res.end(`Hello, ${greet}`);
 }
 
 const port = 8080;
